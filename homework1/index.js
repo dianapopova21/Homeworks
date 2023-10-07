96241daff3c8aca7886aeeb139fbb928525bba73
dianapopova21 <jjolyne.21@gmail.com>
2023-10-07 15:46:16 +0300

    first commit

---
+++ homework1/index.js
@@ -0,0 +1,6 @@
+alert('Hello!');
+let name = prompt('What is your name?');
+let yearOfBirth = prompt('What year were you born?');
+let age = 2023 - yearOfBirth;
+let country = prompt('Where are you from?');
+alert('Nice to meet you, ' + name + '! You are ' + age + ' years old. You are from ' + country + '! :)');
